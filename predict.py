1f3c129997686825d56857bdc8863d944a57de5a
zhaojiyang <zhaojiyang@zhaojiyangs-MacBook-Pro.local>
2023-02-11 13:26:23 +0800

    Add prediction files

---
+++ predict.py
@@ -0,0 +1,25 @@
+#-----------------------------------------------------------------------#
+#   predict.py将单张图片预测、摄像头检测、FPS测试和目录遍历检测等功能
+#   整合到了一个py文件中，通过指定mode进行模式的修改。
+#-----------------------------------------------------------------------#
+import time
+
+import cv2
+import numpy as np
+from PIL import Image
+
+from cyclegan import CYCLEGAN
+
+if __name__ == "__main__":
+    cyclegan = CYCLEGAN()
+    mode = "predict"
+    while True:
+        img = input('Input image filename:')
+        try:
+            image = Image.open(img)
+        except:
+            print('Open Error! Try again!')
+            continue
+        else:
+            r_image = cyclegan.detect_image(image)
+            r_image.show()
